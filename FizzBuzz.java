20b718ecb3af0043c226bc9c4e37d553ce15cd4c
Josué Estévez Fernández <jestfer6@gmail.com>
2018-04-17 13:27:10 +0100

    fizzbuzz, next: find out testing framework of preference and TDD

---
+++ FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+  public static void main(String[] args) {
+    for (int i = 1; i < 100; i++) {
+      String output = "";
+
+      if (i % 3 == 0) output = "Fizz";
+      if (i % 5 == 0) output = "Buzz";
+      if (i % 15 == 0) output = "FizzBuzz";
+
+      System.out.println(output.isEmpty() ? i : output);
+    }
+  }
+}
